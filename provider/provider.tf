4e946fe66ee7876e2f49d866d4ba0f0e58b06402
Aditya Ramesh <nq10023218.external@telefonica.com>
2020-12-16 19:28:57 +0530

    second

--- provider/provider.tf
+++ provider/provider.tf
@@ -9,13 +9,19 @@ provider "aws" {
 }
 
 resource "aws_instance" "first" {
-  provider = "aws.europe"
+  provider = aws.europe
   ami = "ami-0bd39c806c2335b95"
   instance_type = "t2.micro"
+  tags = {
+    Name = "first"
+  }
 }
 
 resource "aws_instance" "second" {
-  provider = "aws.us"
+  provider = aws.us
   ami = "ami-04d29b6f966df1537"
   instance_type = "t2.micro"
+  tags = {
+    Name = "second"
+  }
 }
